ec257294bd57c4dd36d2d9b06f6e2b60d42cc8ec
Timothy Graf <timothygraf@timothys-mbp.home>
2020-06-04 16:08:22 -0700

    add home page and nav component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export const Home = () => {
-    return (
-        <div>
-            
-        </div>
+    const username = useSelector(state => state.username);
+    const text = username ? (
+        <h1>{username} is currently logged in.</h1>
+    ) : (
+        <h1>No one is logged in.</h1>
     );
+
+    return <div>{text}</div>;
 };
 
 export default Home;
