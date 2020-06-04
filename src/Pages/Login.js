d0f79ace2447addba49e8e7155ccd2787313d717
Timothy Graf <timothygraf@timothys-mbp.home>
2020-06-03 18:55:07 -0700

    adding actions and pages and components

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import userActions from '../redux/actions';
 
-export const Login = () => {
+export const LoginPage = props => {
+    // Initializing dispatch
+    const dispatch = useDispatch();
+    // Setting up local state using the useState hook
+    const [loginForm, setLoginForm] = useState({
+        username: '',
+        password: ''
+    });
+
+    // Controled form functions
+    const handleSubmit = e => {
+        e.preventDefault();
+        dispatch(userActions.loginUserToDB(loginForm));
+        props.history.push('/');
+    };
+
+    const handleChange = e => setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+
+    // Destructuring keys from our local state to use in the from
+    const { username, password } = loginForm;
+
+    // Component code
     return (
-        <div>
-            
-        </div>
-    )
-}
+        <form onSubmit={handleSubmit}>
+            <h1>Login Page</h1>
+            <input 
+                type="text"
+                name="username"
+                value={username}
+                onChange={handleChange}
+                placeholder="Username"
+            />
+            <input 
+                type="password"
+                name="passwrod"
+                value={password}
+                placeholder="Password"
+            />
+            <input type="submit" />
+        </form>
+    );
+};
---
+++ src/Routes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Switch, Route} from 'react-router-dom';
+import Pages from './Pages';
+
+export const Routes = () => {
+    return (
+        <Switch>
+            <Route path="/signup" component={Pages.Signup}/>
+            <Route path="login" component={Pages.Login}/>
+            <Route path="home" component={Pages.Home} />
+        </Switch>
+    );
+};
